60f088ff95048bf7b1b92124bdc5c9a65dec2abc
Mihir <cse210001050@iiti.ac.in>
2023-04-21 03:07:44 +0530

    news section added

---
+++ lib/home/new_input.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Newinput extends StatefulWidget {
+  const Newinput({super.key});
+
+  @override
+  State<Newinput> createState() => _NewinputState();
+}
+
+class _NewinputState extends State<Newinput> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+
+    );
+  }
+}
