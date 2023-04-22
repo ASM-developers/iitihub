00133d689f50238984ead2c761062cd3586c0b03
A5HU705H <https://github.com/A5HU705H>
2023-04-23 00:41:22 +0530

    "retrying to build widget"

---
+++ lib/Emails/MessageList.dart
@@ -0,0 +1,7 @@
+import 'package:firstapp/Emails/MessagePage.dart';
+import 'package:firstapp/services/gmail.dart';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+import 'package:googleapis/gmail/v1.dart' as gmail;
---
+++ lib/Emails/MessagePage.dart
@@ -0,0 +1,6 @@
+import 'dart:convert';
+import 'dart:core';
+import 'dart:collection';
+import 'package:firstapp/services/gmail.dart';
+import 'package:googleapis/gmail/v1.dart' as gmail;
+import 'package:flutter/material.dart';
